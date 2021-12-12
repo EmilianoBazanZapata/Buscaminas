50f290b78bcf3083b499220c13a3c58b12c8c1de
Emiliano Bazán-Zapata <emilianobz546@gmail.com>
2021-12-12 21:09:02 +0100

    Agregando el Canvas de GameOver

--- Buscaminas/Assets/Scripts/GameManager.cs
+++ Buscaminas/Assets/Scripts/GameManager.cs
@@ -2,8 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//enumerado
+//posibles estado del juego
+public enum GameState
+{
+    InGame,
+    GameOver,
+    Menu
+}
 public class GameManager : MonoBehaviour
 {
+    //variable que hace referencia al manager
+    public static GameManager SharedInstance;
+    //variable para indicar en que estado se encuentra el juego
+    public GameState CurrentGameState = GameState.Menu;
+
+    private void Awake()
+    {
+        SharedInstance = this;
+    }
+    
     //metodo para finalizar la aplicacion
     public void QuitGame()
     {
